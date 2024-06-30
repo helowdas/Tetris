7f6bffffb333d2b97e2c2a9e6d12098f4387e4ab
TiagoCoz <santiagocozier@gmail.com>
2024-06-30 16:50:25 -0400

    preview.py

--- diseño tablero/preview.py
+++ diseño tablero/preview.py
@@ -9,3 +9,4 @@ class Preview:
 
     def run(self):
         self.display_surface.blit(self.surface,self.rect)
+        pygame.draw.rect(self.display_surface, blanco, self.rect, 2, 2)  #BORDE DEL TABLERO
